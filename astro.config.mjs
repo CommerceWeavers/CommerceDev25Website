bae1735c508a1e456edf7b076ace87233259d844
kulczy <sz.kulczy@gmail.com>
2025-02-25 20:44:02 +0100

    Remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://commerceweavers.github.io',
-  base: '/CommerceDev25Website',
   vite: {
     plugins: [tailwindcss()],
   },
